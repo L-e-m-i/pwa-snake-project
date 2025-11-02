fd16f83ef30e33a3af953278f682265f3101fa68
Lemi <152859505+L-e-m-i@users.noreply.github.com>
2025-11-02 23:21:09 +0100

    Delete src/environments directory

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig ={
-    apiKey: "AIzaSyCKUxVuw9OS5kOW2mp_d04lhQUILEHYtUA",
-    authDomain: "pwa-snake-project.firebaseapp.com",
-    projectId: "pwa-snake-project",
-    storageBucket: "pwa-snake-project.firebasestorage.app",
-    messagingSenderId: "532724539079",
-    appId: "1:532724539079:web:9f6aade9a35c77649e28e2",
-    measurementId: "G-SBF82X3SDJ"
-}
